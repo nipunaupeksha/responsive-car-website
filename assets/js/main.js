cc2ff9d4af16aafad849170b595632d166b14a5f
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-05-15 11:54:14 +0530

    few changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,7 @@ var swiperPopular = new Swiper(".popular__container", {
   },
   breakpoints: {
     768: {
-      slidesPerView: 3
+      slidesPerView: 3,
     },
     1024: {
       spaceBetween: 48,
@@ -59,11 +59,54 @@ var swiperPopular = new Swiper(".popular__container", {
 });
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
-
+let mixerFeatured = mixitup(".featured__content", {
+  selectors: {
+    target: ".featured__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
 /* Link active featured */
+const linkFeatured = document.querySelectorAll(".featured__item");
+
+function activeFeatured() {
+  linkFeatured.forEach((l) => l.classList.remove(".active-featured"));
+  this.classList.add(".active-featured");
+}
+
+linkFeatured.forEach((l) => l.addEventListener("click", activeFeatured));
 
 /*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 /*=============== SCROLL REVEAL ANIMATION ===============*/
